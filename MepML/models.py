3db5e3af594360fc4c91e50de503d9e3d811c1ad
Emanuel <emanuel.gmarques@ua.pt>
2023-03-20 11:32:49 +0000

    created Entities in Models.py

---
+++ MepML/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+
+class Professor(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=50)
+
+class Student(models.Model):
+    mecanographic_number = models.AutoField()
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=50)
+
+class Class(models.Model):
+    name = models.CharField(max_length=50)
+    number_of_students = models.IntegerField()
+    #relationships
+    created_by = models.ForeignKey(Professor)
+    studenst = models.ManyToManyField(Student)
+
+class Dataset(models.Model):
+    path_to_train_file = models.CharField(max_length=200)
+    path_to_test_file  = models.CharField(max_length=200)
+    number_of_features = models.IntegerField()
+    description = models.CharField(max_length=100)
+    is_public = models.BooleanField()
+
+class Metric(models.Model):
+    name = models.CharField(max_length=20)
+    path_to_function = models.CharField(max_length=200)
+
+class Asssignment(models.Model):
+    title = models.CharField(max_length=30)
+    subtitle = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
+    start_date = models.DateTimeField()
+    deadline = models.DateTimeField()
+    limit_of_attempts = models.SmallIntegerField()
+    visibility = models.BooleanField()
+    #relationships
+    created_by = models.ForeignKey(Professor)
+    metrics = models.ManyToManyRel(Metric)
+    dataset = models.ForeignKey(Dataset)
