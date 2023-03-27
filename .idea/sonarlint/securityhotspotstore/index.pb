51de76dbe971819b64af6dc73c1173987b000cbf
RafaelGoncalvesUA <rfg@ua.pt>
2023-03-27 20:16:12 +0100

    refactor project folder

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,13 @@
+
+9
+	manage.py,3/1/3156ad13e4d695cd526bbb7b031016ecba842270
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+6
+run.sh,3/2/324fcb49f1cf313ebdd7cbda6a71b73727ddd94d
+:
+
+Dockerfile,6/6/6651ddff6eb82c840ced7c1dddee15c6e1913dd4
+>
+app_properties,5/c/5cdad85af8f013091eb1b92c00938f1e85dbcf16
--- MepML/models.py
+++ MepML/models.py
@@ -5,28 +5,33 @@ class Professor(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=50)
 
+
 class Student(models.Model):
     mecanographic_number = models.IntegerField()
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=50)
 
+
 class Class(models.Model):
     name = models.CharField(max_length=50)
     number_of_students = models.IntegerField()
-    #relationships
+    # relationships
     created_by = models.ForeignKey(Professor, on_delete="cascade")
     studenst = models.ManyToManyField(Student)
 
+
 class Dataset(models.Model):
     path_to_train_file = models.CharField(max_length=200)
-    path_to_test_file  = models.CharField(max_length=200)
+    path_to_test_file = models.CharField(max_length=200)
     number_of_features = models.IntegerField()
     description = models.CharField(max_length=100)
     is_public = models.BooleanField()
 
+
 class Metric(models.Model):
     name = models.CharField(max_length=20)
     path_to_function = models.CharField(max_length=200)
+
 
 class Asssignment(models.Model):
     title = models.CharField(max_length=30)
@@ -36,7 +41,7 @@ class Asssignment(models.Model):
     deadline = models.DateTimeField()
     limit_of_attempts = models.SmallIntegerField()
     visibility = models.BooleanField()
-    #relationships
+    # relationships
     created_by = models.ForeignKey(Professor, on_delete="cascade")
-    #metrics = models.ManyToManyRel(Metric)
+    # metrics = models.ManyToManyRel(Metric)
     dataset = models.ForeignKey(Dataset, on_delete="cascade")
