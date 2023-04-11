3869d987ac0d6e01391100b5435dda08bcae6238
MagalhaesD77 <97603959+MagalhaesD77@users.noreply.github.com>
2023-04-11 16:51:47 +0100

    Update settings.py

--- djangoMepML/settings.py
+++ djangoMepML/settings.py
@@ -82,22 +82,22 @@ WSGI_APPLICATION = 'djangoMepML.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
 
-    # 'default': {  
-    #     'ENGINE': 'django.db.backends.mysql',  
-    #     'NAME': 'my_mep_ml',  
-    #     'USER': 'root',  
-    #     'PASSWORD': 'mep_ml',  
-    #     'HOST': '34.170.116.176',  
-    #     'PORT': '3306'
-    # }
+     'default': {  
+         'ENGINE': 'django.db.backends.mysql',  
+         'NAME': 'my_mep_ml',  
+         'USER': 'root',  
+         'PASSWORD': 'mep_ml',  
+         'HOST': '34.170.116.176',  
+         'PORT': '3306'
+     }
 
-    'default': {    #Database used for offline development
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-            'OPTIONS': {
-                'timeout': 20,
-            }
-    } 
+    #'default': {    #Database used for offline development
+    #        'ENGINE': 'django.db.backends.sqlite3',
+    #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #        'OPTIONS': {
+    #            'timeout': 20,
+    #        }
+    #} 
 }
 
 # Media files (images, pdfs, etc.)
