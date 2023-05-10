3e56cb059a3a2033305be8f274ba6b24f87c2777
Emanuel Marques <emanuel.gmarques@ua.pt>
2023-05-10 23:53:24 +0100

    CHEK-106 Forgot to remove on original (#29)
    * test dataset is now splitted
    
    * CHEK-106 forgot to remove on original

--- MepML/webservices/ws_exercises.py
+++ MepML/webservices/ws_exercises.py
@@ -18,27 +18,30 @@ def get_exercises(request, prof_id):
 
 
 def post_exercise(request):
-    y_column_file = open(request.FILES['test_dataset'].name[:-4] + "_y.csv", "r+")
+    x_column_file = open(request.FILES['test_dataset'].name, "w+")
+    y_column_file = open(request.FILES['test_dataset'].name[:-4] + "_y.csv", "w+")
     reading_header = True
     for line in request.FILES['test_dataset']:
         line = line.decode("utf-8") 
-        print(type(line), line.strip().split(","))
         if reading_header:
             reading_header = False
             continue
+        x_column_file.write("".join(line.strip().split(",")[:-1]) + "\n")
         y_column_file.write(line.strip().split(",")[-1] + "\n")
-    django_file = File(y_column_file)
+    django_file_x = File(x_column_file)
+    django_file_y = File(y_column_file)
 
     dataset = Dataset.objects.create(
         train_name = request.FILES['train_dataset'].name,
         train_dataset = request.FILES['train_dataset'],
         train_size = request.FILES['train_dataset'].size,
         test_name = request.data['test_dataset'].name,
-        test_dataset = request.FILES['test_dataset'],
-        test_size = request.FILES['test_dataset'].size,
-        test_ground_truth_name = django_file.name,
-        test_ground_truth_file = django_file
+        test_dataset = django_file_x,
+        test_size = django_file_x.size,
+        test_ground_truth_name = django_file_x.name,
+        test_ground_truth_file = django_file_y
     )
+    x_column_file.close()
     y_column_file.close()
     data_ = request.data
     data_['dataset'] = dataset.id
