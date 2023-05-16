be31821ac6d970c58e787f922fe3b44f9999cd60
Emanuel Marques <emanuel.gmarques@ua.pt>
2023-05-16 23:14:00 +0100

    firebase auth now with API (#39)

--- djangoMepML/settings.py
+++ djangoMepML/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import posixpath
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,8 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6fd3fa88-291e-4579-9e3a-541f3c1d92f5'
+SECRET_KEY = os.environ.get("django-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
