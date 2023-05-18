b1569ad71d36aa401ea4b9bdbf86207c41c25294
Emanuel Marques <emanuel.gmarques@ua.pt>
2023-05-18 10:13:54 +0100

    Update models.py

--- MepML/models.py
+++ MepML/models.py
@@ -50,7 +50,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
 
-    firebase_uuid = models.CharField(max_length=64, defualt='')
+    firebase_uuid = models.CharField(max_length=64, default='')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['nmec', 'name']
